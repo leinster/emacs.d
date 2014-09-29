ce4a7f84ee9dbfd0e7879af8535723b9ee9fcd41
Jon Ramsey <jonathon.ramsey@gmail.com>
2014-09-29 16:39:00 +0100

    Formatting.

--- init.el
+++ init.el
@@ -68,7 +68,8 @@
 
 (dolist (f '(try-expand-line try-expand-list try-complete-file-name-partially))
   (delete f hippie-expand-try-functions-list))
-(add-to-list 'hippie-expand-try-functions-list 'try-complete-file-name-partially t)
+(add-to-list 'hippie-expand-try-functions-list
+             'try-complete-file-name-partially t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; packages
