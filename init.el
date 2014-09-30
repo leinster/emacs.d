8231d3c15a15a337e70ee57689c3338bf8e460bb
Jon Ramsey <jonathon.ramsey@gmail.com>
2014-09-30 01:38:53 +0100

    Doc tweak.

--- init.el
+++ init.el
@@ -138,7 +138,7 @@
 
 ;; http://milkbox.net/note/single-file-master-emacs-configuration/
 (defmacro after (mode &rest body)
-  "`eval-after-load' MODE evaluate BODY."
+  "`eval-after-load' `MODE' evaluate `BODY'."
   (declare (indent defun))
   `(eval-after-load ,mode
      '(progn ,@body)))
